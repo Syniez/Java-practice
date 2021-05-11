2c797fc19fa58619bf963b0d65506bb8f0805b2a
Syniez <57395302+Syniez@users.noreply.github.com>
2021-05-11 15:19:29 +0900

    Update Inherit.java

--- Inherit.java
+++ Inherit.java
@@ -7,7 +7,7 @@ class super_sample {
     }
 
     public super_sample(String name) {
-        this.name = name;
+        this.name = name;                                                      // 전역변수와 지역변수를 구분하기위해 this.가 쓰임
         System.out.println("스트링을 사용하는 슈퍼 클래스 생성자 입니다.");
         System.out.println(name + "님 안녕하세요");
     }
@@ -25,7 +25,7 @@ class child_sample extends super_sample {
     }
 
     public child_sample(String name) {
-        super(name);
+        super(name);                                                           // 생성자에 argument가 있을때는 super()가 자동으로 안되기때문에 super(argument)가 필요!
         System.out.println("스트링을 사용하는 자식 클래스 생성자 입니다.");
     }
 }
