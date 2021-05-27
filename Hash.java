7299643d2eee99f20f0909d085258eb2282b9135
Syniez <57395302+Syniez@users.noreply.github.com>
2021-05-27 20:33:04 +0900

    Add files via upload

---
+++ Hash.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"a", "b", "c", "d"};
+        Set<String> sample = new HashSet<>();
+        for (String a:arr)
+            sample.add(a);
+        System.out.println(sample);
+
+        Set<String> Myset = new HashSet<>();
+    }
+}
